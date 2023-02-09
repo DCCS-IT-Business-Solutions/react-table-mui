a4b7801c70cf1f0378460b2ea52f972e9703b266
Stephan Meißner <stephan.meissner@dccs.at>
2023-02-09 20:04:46 +0100

    typed the whole thing.

---
+++ src/IColDef.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+export interface IColDef<T> {
+  prop: string;
+  header: string | ElementContent<T> | React.ReactNode;
+  accessor?: (data: T) => string;
+  width?: number;
+  render?: (data: T) => React.ReactNode;
+  renderFilter?: (
+    value: T,
+    handleChange: (v: T) => void
+  ) => React.ReactNode;
+  props?: (data: T) => object;
+  headerProps?: object;
+  footerProps?: object;
+  sortable?: boolean;
+  filterable?: boolean;
+  align?: "left" | "center" | "right";
+  footer?: string | ElementContent<T>;
+}
+
+type Fn<T> = (colDef: IColDef<T>, data: T[]) => React.ReactNode;
+type ElementContent<T> = string | Fn<T>;
